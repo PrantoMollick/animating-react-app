6dc5644ea965e76037f920a9fbfd8b7db1d77592
== <prantocse01@gmail.com>
2022-03-22 17:25:06 +0600

    css transition component

--- src/components/Backdrop/Backdrop.js
+++ src/components/Backdrop/Backdrop.js
@@ -3,8 +3,8 @@ import React from 'react';
 import './Backdrop.css';
 
 const backdrop = (props) => {
-    const cssClass = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed' ]
-    return <div className={cssClass.join(' ') }></div>;
+  const cssClass = ['Backdrop', props.show ? 'BackdropOpen' : 'BackdropClosed'];
+  return <div className={cssClass.join(' ')}></div>;
 };
 
 export default backdrop;
